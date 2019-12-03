c4080c3e7d956ded76433d23d456c4ea1812379f
wayden-developer <wayden.jones1@gmail.com>
2019-12-03 10:24:39 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent,
     CoursesComponent,
     AuthorComponent
   ],
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,22 +4,10 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-  <h2>{{"Title: " + getTitle()}}</h2>
-  <ul>
-    <li *ngFor="let course of courses">
-      {{ course }}
-    </li>
-  </ul>
+
+  
   `
 })
 export class CoursesComponent {
-  title = 'List of courses';
-  courses;
 
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses()
-  }
-  getTitle(){
-    return this.title;
-  }
 }
